4e54136913783004833064d1d3cb68f78ae40991
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2019-09-26 16:33:43 +0300

    changed h1

--- frontend/components/DgSpecialistWorkload/StatisticsTable.vue
+++ frontend/components/DgSpecialistWorkload/StatisticsTable.vue
@@ -6,7 +6,7 @@
         </div>
 
         <b-table-simple small caption-top responsive striped bordered v-else>
-            <caption>Статистика навантаження фахівців за період від {{ dateFrom | moment("DD.MM.YYYY") }} до {{ dateTo | moment("DD.MM.YYYY") }}</caption>
+            <caption>Статистичні дані щодо навантаження фахівців за період від {{ dateFrom | moment("DD.MM.YYYY") }} до {{ dateTo | moment("DD.MM.YYYY") }}</caption>
             <b-thead class="text-center">
                 <b-tr>
                     <b-th rowspan="2">Фахівець</b-th>
